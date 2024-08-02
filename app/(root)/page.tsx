f8c13bce1eba9591346bc73ae79da834a3961440
Gualberto Oyangoren <businessforyang101@gmail.com>
2024-08-03 06:45:42 +0800

    add homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import HeaderBox from '@/components/ui/HeaderBox';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = {firstName: 'Yang'};
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="Greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your cash flow effectively."
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.10}
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
